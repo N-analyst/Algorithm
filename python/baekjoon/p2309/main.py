5df0c3fbf6a173124c27bfd34897490ee2258beb
N-analyst <nanalyst@daum.net>
2026-01-13 23:38:41 +0900

    python baekjoon 2309

---
+++ python/baekjoon/p2309/main.py
@@ -0,0 +1,11 @@
+import sys
+from itertools import combinations
+
+inputs = [int(sys.stdin.readline()) for _ in range(9)]
+
+
+for case in combinations(inputs, 7):
+    if sum(case) == 100:
+        for i in sorted(case):
+            print(i)
+        break
